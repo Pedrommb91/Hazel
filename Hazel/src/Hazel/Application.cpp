c154771600f91533a97b3b6651c10f77657d5b97
Pedro Miguel <pedromiguelmb@gmail.com>
2020-05-29 10:13:20 +0100

    Added Timestep

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,6 +3,8 @@
 #include "Hazel/Renderer/Renderer.h"
 #include "Input.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -25,8 +27,12 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
+			float time = (float)glfwGetTime(); // Platform::GetTime()
+			Timestep timestep = time - m_LastFrameTime;
+			m_LastFrameTime = time;
+
 			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate();
+				layer->OnUpdate(timestep);
 
 			/*m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
