1dc2096f0c447eb6735f00b5ef02fb02992556f1
Pedro Miguel <pedromiguelmb@gmail.com>
2020-06-01 23:31:43 +0100

    Shaders can now be read from files.

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,7 @@
-call vendor\bin\premake\premake5.exe vs2019
+@echo off
+call vendor\bin\premake\premake5.exe vs2017
+call vendor\bin\premake\premake5.exe vs2017
 PAUSE
+IF %ERRORLEVEL% NEQ 0 (
+  PAUSE
+)
